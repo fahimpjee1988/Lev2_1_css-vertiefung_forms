7fa9f3bf77d684382ab6905a7df544bed81e0e3e
fahimpjee1988 <adjmal.ra@gmail.com>
2020-06-17 14:40:30 +0200

    cssandjava

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let name = document.getElementById('tel')
+let email = document.getElementById('email')
+let tel = document.getElementById('tel')
+
+function showInput() {
+    console.log("it works")
+    console.log(name.value)
+    console.log(email.value)
+    console.log(tel.value)
+}
+
